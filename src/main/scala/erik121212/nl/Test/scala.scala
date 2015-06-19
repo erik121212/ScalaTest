d6383e9aeddfa39c9695b44e1b9af20c4c954940
Erik de Nooij <Erikdenooij@hotmail.com>
2015-06-19 22:15:18 +0200

    added libs

---
+++ src/main/scala/erik121212/nl/Test/scala.scala
@@ -0,0 +1,14 @@
+package erik121212.nl.Test
+
+import akka.actor.{ActorSystem, Props, actorRef2Scala}
+
+
+
+object scala {
+  def main(args: Array[String]) {
+    System.out.println("test2")
+
+    val system = ActorSystem("PrimeNumbers")
+  }
+
+}
